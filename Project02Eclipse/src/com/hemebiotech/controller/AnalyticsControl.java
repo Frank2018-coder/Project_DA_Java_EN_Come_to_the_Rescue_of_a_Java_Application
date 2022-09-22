cc6bfc4966f43175e2fc326b94098e713df851c0
Frank2018-coder <frankmboumda2021@gmail.com>
2022-09-22 05:12:30 +0200

    Fin des tests du code

---
+++ Project02Eclipse/src/com/hemebiotech/controller/AnalyticsControl.java
@@ -0,0 +1,36 @@
+package com.hemebiotech.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import com.hemebiotech.dao.IProcesData;
+import com.hemebiotech.dao.ISymptomReader;
+import com.hemebiotech.dao.IWriteData;
+import com.hemebiotech.dao.ProcesData;
+import com.hemebiotech.dao.ReadSymptomDataFromFile;
+import com.hemebiotech.dao.WriteData;
+
+
+public class AnalyticsControl {
+    /**
+     * implements the control logic of the operations of reading, 
+     * counting, sorting and writing of the list of symptoms
+     */
+	public void run() {
+		//file read logic
+		ISymptomReader readerSymptomList = new ReadSymptomDataFromFile("symptoms.txt");
+		List<String> list = readerSymptomList.GetSymptoms();
+		 for(String l : list) {
+			 System.out.println(l+"\n");
+		 }
+		 
+		 //proces
+		 IProcesData iProces = new ProcesData();
+		 Map<String, Integer> map = iProces.countAndSort(list);
+		 System.out.println(map);
+		 
+		 //writing
+		 IWriteData write = new WriteData();
+		 write.write(map);
+	}
+}
